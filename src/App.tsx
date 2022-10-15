4ab1cb9eb55504868a9f20b9b723f8a16ae5a65d
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-10-15 10:49:38 +0200

    Addition of the Form component to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import Form from "./components/Form/Form";
 
 const App = () => {
   return (
     <div className="container">
       <div className="card__container"></div>
-      <div className="form__container"></div>
+
+      <div className="form__container">
+        <Form />
+      </div>
     </div>
   );
 };
