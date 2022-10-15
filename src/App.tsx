63553b11417024acfc193e3a8a1d2a8c29bb79f6
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-10-15 15:27:35 +0200

    Addition of the CreditCard component to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import CreditCard from "./components/CreditCards/CreditCard";
 import Form from "./components/Form/Form";
 
 const App = () => {
   return (
     <div className="container">
-      <div className="card__container"></div>
+      <div className="card__container">
+        <div className="card__background">
+          <CreditCard />
+        </div>
+      </div>
 
       <div className="form__container">
         <Form />
