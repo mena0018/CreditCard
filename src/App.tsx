9a38ff8d071d57ea8e5e49eddb8b81c5995423b7
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-10-18 21:14:58 +0200

    Setup the animation to the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import CreditCard from "./components/CreditCards/CreditCard";
 import Form from "./components/Form/Form";
+import { useEffect, useRef} from "react";
+import gsap from "gsap";
+
 
 const App = () => {
+
+  const formRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    gsap.fromTo(formRef.current,
+      { opacity: 0, y: -150 }, 
+      { opacity: 1, y: 0, duration: 0.6, delay: 0.5 }
+   );
+  })
+
   return (
     <div className="container">
       <div className="card__container">
@@ -10,7 +23,7 @@ const App = () => {
         </div>
       </div>
 
-      <div className="form__container">
+      <div className="form__container" ref={formRef}>
         <Form />
       </div>
     </div>
