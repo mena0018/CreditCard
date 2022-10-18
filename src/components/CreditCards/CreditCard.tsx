c5482c66c7735c8ec965fca3ef6493b67fbcbcd1
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-10-18 21:14:30 +0200

    Setup the animation for both credit card

--- src/components/CreditCards/CreditCard.tsx
+++ src/components/CreditCards/CreditCard.tsx
@@ -1,11 +1,30 @@
 import styles from "./CreditCard.module.scss";
+import { useEffect, useRef} from "react";
+import gsap from "gsap";
+
 
 const CreditCard = () => {
+
+  const frontCardRef = useRef<HTMLDivElement>(null);
+  const backCardRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    gsap.fromTo(frontCardRef.current,
+       { opacity: 0, x: -150 }, 
+       { opacity: 1, x: 0, duration: 0.6 }
+    );
+
+    gsap.fromTo(backCardRef.current,
+      { opacity: 0, x: -150 }, 
+      { opacity: 1, x: 0, duration: 0.6, delay: 0.3 }
+   );
+  }, [])
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.back_card_container}></div>
+      <div className={styles.back_card_container} ref={backCardRef}></div>
 
-      <div className={styles.front_card_container}>
+      <div className={styles.front_card_container} ref={frontCardRef}>
         <div className={styles.card_header}></div>
 
         <div className={styles.card_middle}>
