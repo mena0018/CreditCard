876f207c4af6bab18e12f67114f058c134dac0ee
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-10-23 19:54:40 +0200

    Gestion when the form is submitted

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,14 +1,18 @@
 import styles from "./Form.module.scss";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { FormTypes } from "../../types/FormTypes";
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { FormContext } from "../../contexts/formContext";
-
+import validationIcon from "../../images/icon-complete.svg";
 
 const Form = () => {
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const { register, handleSubmit, formState: { errors } } = useForm<FormTypes>();
-  const onSubmit: SubmitHandler<FormTypes> = (data:FormTypes, e) => clearDatas(e);
+  const onSubmit: SubmitHandler<FormTypes> = (data:FormTypes, e) => {
+    clearDatas(e)
+    setIsSubmit(true)
+  };
 
   const { formData, setFormData } = useContext(FormContext);
 
@@ -24,10 +28,11 @@ const Form = () => {
     e.target[1].value = '';  
     e.target[2].value = '';  
     e.target[3].value = '';  
+    e.target[4].value = '';  
 
     setFormData({
       name: "JANE APPLESEED",
-      cardNumber: "0000 0000 0000 0000",
+      cardNumber: "0000000000000000",
       month: "00",
       year: "00",
       cvc: "000",
@@ -35,67 +40,80 @@ const Form = () => {
   }
   
   return (
-    <form className={styles.form__content} onSubmit={handleSubmit(onSubmit)}>
+    <>
+      <form className={!isSubmit ? styles.form__content : styles.none} onSubmit={handleSubmit(onSubmit)}>
 
-      <label htmlFor="name">CARDHOLDER NAME</label>
-      <input placeholder="e.g. Jane Appleseed" id="name"
-        {...register("name", {
-           required: true,
-           onChange: (e) => changeHandler("name", e.target.value),
-        })} />
-      <p>{errors.name ? "Can't be blank": ""}</p>
+        <label htmlFor="name">CARDHOLDER NAME</label>
+        <input placeholder="e.g. Jane Appleseed" id="name"
+          {...register("name", {
+            required: true,
+            onChange: (e) => changeHandler("name", e.target.value),
+          })} />
+        <p>{errors.name ? "Wrong format, letters only": ""}</p>
 
-      <label htmlFor="number">CARD NUMBER</label>
-      <input id="number" placeholder="e.g. 1234 5678 9123 0000" 
-        {...register("cardNumber", {
-          onChange: (e) => changeHandler("cardNumber", e.target.value),
-          required: true, 
-          minLength: 16,
-          maxLength: 16
-        }
-        )}/>
-      <p>{errors.cardNumber ? "the card number must have 16 characters": ""}</p>
+        <label htmlFor="number">CARD NUMBER</label>
+        <input id="number" placeholder="e.g. 1234 5678 9123 0000"
+          {...register("cardNumber", {
+            onChange: (e) => changeHandler("cardNumber", e.target.value),
+            required: true, 
+            minLength: 16,
+            maxLength: 16
+          }
+          )}/>
+        <p>{errors.cardNumber ? "The card number must have 16 characters": ""}</p>
 
-      <div className={styles.footer}>
-        <div className={styles.date__expire}>
-          <label htmlFor="expiration-date">EXP. DATE (MM/YY)</label>
+        <div className={styles.footer}>
+          <div className={styles.date__expire}>
+            <label htmlFor="expiration-date">EXP. DATE (MM/YY)</label>
 
-          <div className={styles.input_container}>
-            <input id="expiration-date" placeholder="MM" 
-              {...register("month", {
+            <div className={styles.input_container}>
+              <input id="expiration-date" placeholder="MM"
+                {...register("month", {
+                  required: true,
+                  minLength: 2,
+                  maxLength: 2,
+                  onChange: (e) => changeHandler("month", e.target.value),
+                }
+                )}/>
+
+              <input placeholder="YY"
+                {...register("year", {
+                  required: true,
+                  minLength: 2,
+                  maxLength: 2,
+                  onChange: (e) => changeHandler("year", e.target.value),
+                }
+                )}/>
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="cvc" >CVC</label>
+            <input type="number" id="cvc" placeholder="e.g. 123"
+              {...register("cvc", {
                 required: true,
-                minLength: 2,
-                maxLength: 2,
-                onChange: (e) => changeHandler("month", e.target.value),
-              }
-              )}/>
-
-            <input placeholder="YY" 
-              {...register("year", {
-                required: true,
-                minLength: 2,
-                maxLength: 2,
-                onChange: (e) => changeHandler("year", e.target.value),
+                minLength: 3,
+                maxLength: 3,
+                onChange: (e) => changeHandler("cvc", e.target.value),
               }
               )}/>
           </div>
         </div>
 
-        <div>
-          <label htmlFor="cvc" >CVC</label>
-          <input type="number" id="cvc" placeholder="e.g. 123" 
-            {...register("cvc", {
-              required: true,
-              minLength: 3,
-              maxLength: 3,
-              onChange: (e) => changeHandler("cvc", e.target.value),
-            }
-            )}/>
-        </div>
-      </div>
+        <button>Confirm</button>
+      </form>
 
-      <button>Confirm</button>
-    </form>
+      <div className={isSubmit ? styles.form__submit : styles.none}>
+        <div className={styles.validation__icon}>
+          <img src={validationIcon}/>
+        </div>
+        
+        <h1>Thank you!</h1>
+        <p>We've added your card details</p>
+
+        <button onClick={() => setIsSubmit(false)}>Continue</button>
+      </div>
+    </>
   );
 };
 
